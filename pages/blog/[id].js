146cf0709bec42779a305605e2ca7a7e290d8055
Monica Delgado <mnca.delgado@gmail.com>
2022-07-26 17:52:05 -1100

    deleted extra comments

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -2,17 +2,6 @@ import { fire } from "../../config/fire-config.js"
 import { useState, useEffect } from "react"
 import Link from "next/link"
 
-/*
-CREATE BLOG ROUTES, READ BLOG ENTRY
-1.How to create a dynamic url form the index page for each blog entry 
-  we did this in the index.js files 
-2.Single blog entries --> read up on the different renders in Next.js documentation.
-  We're using getInitialProps
-NOTE: the props are not being read (code no working) I think is because this is 
-a client side request, also I don't know where are the props coming from 
-
-*/
-
 const Blog = (props) => {
   const [blog, setBlog] = useState(null)
   console.log(blog)
@@ -45,7 +34,6 @@ const Blog = (props) => {
 }
 
 Blog.getInitalProps = ({ query }) => {
-  //This is client side rendering? .getInitialProps
   console.log(query.id)
   return {
     id: query.id,
