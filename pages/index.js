b1b01da8294f1ae692f716b51fd286611981fe29
Monica Delgado <mnca.delgado@gmail.com>
2022-07-26 22:41:36 -1100

    added log in and log out

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,21 @@ READING FROM THE CLOUD FIRESTORE AND DISPLAYING THE DATA ON THE UI
 
 */
 
+/*
+Sing in and sing out user
+*/
+
 const Home = () => {
   const [blogs, setBlogs] = useState([])
-
+  const [notification, setNotification] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false)
+  fire.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+  })
   useEffect(() => {
     fire
       .firestore()
@@ -31,25 +43,47 @@ const Home = () => {
         setBlogs(blogs)
       })
   }, [])
-
-  console.log(blogs)
-
+  const handleLogout = () => {
+    fire
+      .auth()
+      .signOut()
+      .then(() => {
+        setNotification("Logged out")
+        setTimeout(() => {
+          setNotification("")
+        }, 2000)
+      })
+  }
   return (
     <div>
       <Head>
         <title>Blog App</title>
       </Head>
       <h1>Blog</h1>
+      {notification}
+      {!loggedIn ? (
+        <div>
+          <Link href="/users/register">
+            <a>Register</a>
+          </Link>{" "}
+          |
+          <Link href="/users/login">
+            <a> Login</a>
+          </Link>
+        </div>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
             <Link href="/blog/[id]" as={"/blog/" + blog.id}>
-              <a>{blog.title}</a>
+              <a itemProp="hello">{blog.title}</a>
             </Link>
           </li>
         ))}
       </ul>
-      <CreatePost />
+      {loggedIn && <CreatePost />}
     </div>
   )
 }
